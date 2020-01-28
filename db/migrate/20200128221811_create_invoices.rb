a020cb480ffbe9623c87dcaf082d0ee2ce951974
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-28 15:19:05 -0700

    Create Invoice model, factory, and migration

---
+++ db/migrate/20200128221811_create_invoices.rb
@@ -0,0 +1,11 @@
+class CreateInvoices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :invoices do |t|
+      t.text :status
+      t.references :customer, null: false, foreign_key: true
+      t.references :merchant, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
