51d25ec69593f9bbd3c8b134066d9a9bacd6a8c1
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-28 15:42:03 -0700

    Create model test for Transaction model

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :credit_card_number }
+    it { should validate_presence_of :result }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :invoice}
+  end
+
+  describe 'methods' do
+  end
+end
