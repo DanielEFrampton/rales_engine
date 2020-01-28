e3e4dc485f85248ab159a4ff5f8a45c0a39980d3
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-28 15:16:10 -0700

    Update schema after running CreateMerchant migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_204528) do
+ActiveRecord::Schema.define(version: 2020_01_28_221406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_01_28_204528) do
   create_table "customers", force: :cascade do |t|
     t.text "first_name"
     t.text "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
