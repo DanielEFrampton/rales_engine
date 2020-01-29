e144ab667eab2d055bfd9a3861d6a6bad5a5d254
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-29 14:29:43 -0700

    Modify Merchant factory to create random date in range as Datetime

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -3,5 +3,7 @@
 FactoryBot.define do
   factory :merchant do
     name { Faker::Name.name }
+    created_at { Time.zone.parse(Faker::Date.in_date_period.to_s) }
+    updated_at { Time.zone.parse(Faker::Date.in_date_period.to_s) }
   end
 end
