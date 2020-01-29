d62584e6934aaf5b8c652c46264b356e2c525206
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-29 08:55:21 -0700

    Add namespaced merchant route for FindController

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        resource :find, only: [:show], controller: 'find'
+      end
+
       resources :merchants, only: [:index, :show]
     end
   end
