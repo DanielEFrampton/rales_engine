ea7c044f1b5f181ef09b3c852c5052c1a2bde3fe
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-29 21:25:04 -0700

    Create merchant-namespaced Random controller and show action

---
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Merchants::RandomController < ApplicationController
+  def show
+    ids = Merchant.all.pluck(:id)
+    random_id = ids.sample
+    render json: MerchantSerializer.new(Merchant.find(random_id))
+  end
+end
