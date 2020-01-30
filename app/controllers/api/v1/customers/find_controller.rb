f1c255a782b52d038328594f258c1912de2ee755
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-30 16:07:10 -0700

    Refactor customers find controller to break out helper methods

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -3,23 +3,11 @@
 # Controller for Find Single Customer Endpoint
 class Api::V1::Customers::FindController < ApplicationController
   def show
-    if ["first_name", "last_name"].include?(attribute)
-      customer = Customer.ci_find(attribute, value)
-    else
-      customer = Customer.find_by(attribute => value)
-    end
-    render json: CustomerSerializer.new(customer)
+    render json: CustomerSerializer.new(found_records(:find_by))
   end
 
   def index
-    customers = if parameters[:first_name]
-                  Customer.ci_first_name_find_all(parameters[:first_name])
-                elsif parameters[:last_name]
-                  Customer.ci_last_name_find_all(parameters[:last_name])
-                else
-                  Customer.where(parameters)
-                end
-    render json: CustomerSerializer.new(customers)
+    render json: CustomerSerializer.new(found_records(:where))
   end
 
   private
@@ -34,5 +22,13 @@ class Api::V1::Customers::FindController < ApplicationController
 
   def value
     parameters.values.first
+  end
+
+  def found_records(method)
+    if ["first_name", "last_name"].include?(attribute)
+      Customer.ci_find(attribute, value)
+    else
+      Customer.send(method, attribute => value)
+    end
   end
 end
