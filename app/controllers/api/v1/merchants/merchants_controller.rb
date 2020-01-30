33446f1eb0fa4e1318f3fd9aeb7d556aaa88b01c
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-30 11:55:26 -0700

    Refactor merchant endpoints to all be within Merchants namespace

--- app/controllers/api/v1/merchants/merchants_controller.rb
+++ app/controllers/api/v1/merchants/merchants_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Controller for Merchants resource in API/V1 namespace
-class Api::V1::MerchantsController < ApplicationController
+class Api::V1::Merchants::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
+        get '/', to: 'merchants#index'
         get '/find_all', to: 'find#index'
         get '/find', to: 'find#show'
         get '/random', to: 'random#show'
+        get '/:id', to: 'merchants#show'
       end
-
-      resources :merchants, only: [:index, :show]
 
       namespace :customers do
         get '/', to: 'customers#index'
