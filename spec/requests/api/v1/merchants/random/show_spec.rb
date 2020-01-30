92d58eca305a3cc6b8f84f565f2f98cd186ca209
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-29 20:57:01 -0700

    Create request test for merchant-namespaced random show endpoint

---
+++ spec/requests/api/v1/merchants/random/show_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :request do
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @merchant_3 = create(:merchant)
+  end
+
+  describe 'when I send a get request to the merchants random path' do
+    before(:each) do
+      get "/api/v1/merchants/random"
+      @hash = JSON.parse(response.body)
+    end
+
+    it 'I get a JSON:API response containing attributes of a random merchant' do
+      expect(@hash.class).to eq(Hash)
+      expect(@hash.keys).to eq(["data"])
+      expect(@hash["data"].length).to eq(3)
+      expect(@hash["data"].class).to eq(Hash)
+
+      expect(@hash["data"]["id"]).to eq("#{@merchant_1.id}")
+      expect(@hash["data"]["type"]).to eq("merchant")
+      expect(@hash["data"]["attributes"].class).to eq(Hash)
+      expect(@hash["data"]["attributes"].length).to eq(2)
+      expect(@hash["data"]["attributes"]["name"]).to eq("#{@merchant_1.name}")
+      expect(@hash["data"]["attributes"]["id"]).to eq(@merchant_1.id)
+
+      expect(@hash.to_s).not_to include("#{@merchant_2.name}")
+      expect(@hash.to_s).not_to include("#{@merchant_3.name}")
+    end
+  end
+end
