e93904d4a7a55ee0f44cc9f416655f2f4ebd66be
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-30 10:35:29 -0700

    Refactor to assign variable based on conditional per Rubocop docs

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -3,20 +3,20 @@
 # Controller for Find Single Merchant Endpoint
 class Api::V1::Merchants::FindController < ApplicationController
   def show
-    if name = parameters[:name]
-      merchant = Merchant.ci_name_find(name)
-    else
-      merchant = Merchant.find_by(parameters)
-    end
+    merchant = if parameters[:name]
+                 Merchant.ci_name_find(name)
+               else
+                 Merchant.find_by(parameters)
+               end
     render json: MerchantSerializer.new(merchant)
   end
 
   def index
-    if name = parameters[:name]
-      merchants = Merchant.ci_name_find_all(name)
-    else
-      merchants = Merchant.where(parameters)
-    end
+    merchant = if parameters[:name]
+                 merchants = Merchant.ci_name_find_all(name)
+               else
+                 merchants = Merchant.where(parameters)
+               end
     render json: MerchantSerializer.new(merchants)
   end
 
