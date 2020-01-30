7e3eea6a1c4814c3710cc756b5b837193f4e25ee
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-30 11:39:14 -0700

    Refactor merchants random controller show action to use .random

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -3,8 +3,6 @@
 # Controller for Random Merchant endpoint
 class Api::V1::Merchants::RandomController < ApplicationController
   def show
-    ids = Merchant.all.pluck(:id)
-    random_id = ids.sample
-    render json: MerchantSerializer.new(Merchant.find(random_id))
+    render json: MerchantSerializer.new(Merchant.random)
   end
 end
