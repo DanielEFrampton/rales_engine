1d8d1eb18ae7d11300f789d9f423159611a3a492
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-01-30 10:20:44 -0700

    Add magic comment and class description comment per Rubocop

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Controller for Random Merchant endpoint
 class Api::V1::Merchants::RandomController < ApplicationController
   def show
     ids = Merchant.all.pluck(:id)
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Controller for Merchants resource in API/V1 namespace
 class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
