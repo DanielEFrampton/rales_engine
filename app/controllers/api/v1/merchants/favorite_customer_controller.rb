17f2864ef354ad68dafadd7f1b51f1bcc498d8c8
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-01 15:24:23 -0700

    Refactor favorite_customer show action to go through Customer model

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
   def show
-    favorite_customer = Merchant.find(params[:merchant_id]).favorite_customer
+    favorite_customer = Customer.favorite_customer(params[:merchant_id])
     render json: CustomerSerializer.new(favorite_customer)
   end
 end
