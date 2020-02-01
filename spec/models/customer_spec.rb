386c99911b7ce19041a3698b68875f5d50853491
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-01 14:07:54 -0700

    Refactor favorite_customer as Customer class method not Merch instance

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -14,5 +14,33 @@ RSpec.describe Customer, type: :model do
   end
 
   describe 'methods' do
+    describe 'class methods' do
+      describe 'favorite_customer' do
+        it 'returns customer with most successful transactions with merchant' do
+          @merchant = create(:merchant)
+
+          @customer_1 = create(:customer)
+          @customer_2 = create(:customer)
+          @customer_3 = create(:customer)
+
+          @invoice_1 = create(:invoice, merchant: @merchant, customer: @customer_1)
+          @transaction_1 = create(:transaction, invoice: @invoice_1)
+          @invoice_2 = create(:invoice, merchant: @merchant, customer: @customer_1)
+          @transaction_1 = create(:transaction, invoice: @invoice_2)
+
+          @invoice_3 = create(:invoice, merchant: @merchant, customer: @customer_2)
+          @transaction_1 = create(:transaction, invoice: @invoice_3)
+          @invoice_4 = create(:invoice, merchant: @merchant, customer: @customer_2)
+          @transaction_1 = create(:transaction, invoice: @invoice_4)
+          @invoice_5 = create(:invoice, merchant: @merchant, customer: @customer_2)
+          @transaction_1 = create(:transaction, invoice: @invoice_5)
+
+          @invoice_6 = create(:invoice, merchant: @merchant, customer: @customer_3)
+          @transaction_1 = create(:transaction, invoice: @invoice_6)
+
+          expect(Customer.favorite_customer(@merchant.id)).to eq(@customer_2)
+        end
+      end
+    end
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -14,35 +14,6 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'methods' do
-    describe 'instance methods' do
-      describe 'favorite_customer' do
-        it 'returns customer with most successful transactions with merchant' do
-          @merchant = create(:merchant)
-
-          @customer_1 = create(:customer)
-          @customer_2 = create(:customer)
-          @customer_3 = create(:customer)
-
-          @invoice_1 = create(:invoice, merchant: @merchant, customer: @customer_1)
-          @transaction_1 = create(:transaction, invoice: @invoice_1)
-          @invoice_2 = create(:invoice, merchant: @merchant, customer: @customer_1)
-          @transaction_1 = create(:transaction, invoice: @invoice_2)
-
-          @invoice_3 = create(:invoice, merchant: @merchant, customer: @customer_2)
-          @transaction_1 = create(:transaction, invoice: @invoice_3)
-          @invoice_4 = create(:invoice, merchant: @merchant, customer: @customer_2)
-          @transaction_1 = create(:transaction, invoice: @invoice_4)
-          @invoice_5 = create(:invoice, merchant: @merchant, customer: @customer_2)
-          @transaction_1 = create(:transaction, invoice: @invoice_5)
-
-          @invoice_6 = create(:invoice, merchant: @merchant, customer: @customer_3)
-          @transaction_1 = create(:transaction, invoice: @invoice_6)
-
-          expect(@merchant.favorite_customer).to eq(@customer_2)
-        end
-      end
-    end
-
     describe 'class methods' do
       describe 'ci_name_find' do
         it 'returns single record with matching name regardless of case' do
