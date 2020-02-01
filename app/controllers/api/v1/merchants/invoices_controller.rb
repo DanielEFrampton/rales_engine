eb0ef7dbdd9cdc655fdd29b6d71c734b41f277c1
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-01 12:15:59 -0700

    Refactor merchant relationship endpoints to sort records by id

--- app/controllers/api/v1/merchants/invoices_controller.rb
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::InvoicesController < ApplicationController
   def index
-    invoices = Merchant.find(params[:merchant_id]).invoices
+    invoices = Merchant.find(params[:merchant_id]).invoices.order(:id)
     render json: InvoiceSerializer.new(invoices)
   end
 end
