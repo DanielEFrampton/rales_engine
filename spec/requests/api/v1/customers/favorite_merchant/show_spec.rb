d9110a3e9ecb5b64badc05e5a84fb5445fcb3af6
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-02 15:56:10 -0700

    Correct instance variable name in request test

--- spec/requests/api/v1/customers/favorite_merchant/show_spec.rb
+++ spec/requests/api/v1/customers/favorite_merchant/show_spec.rb
@@ -51,17 +51,17 @@ RSpec.describe 'As a visitor', type: :request do
     end
 
     it 'I get JSON for merchant customer successfully transacted most with' do
-      expect(@hash.class).to eq(Hash)
-      expect(@hash.keys).to eq(['data'])
-      expect(@hash['data'].length).to eq(3)
-      expect(@hash['data'].class).to eq(Hash)
+      expect(@json.class).to eq(Hash)
+      expect(@json.keys).to eq(['data'])
+      expect(@json['data'].length).to eq(3)
+      expect(@json['data'].class).to eq(Hash)
 
-      expect(@hash['data']['id']).to eq(@merchant_2.id.to_s)
-      expect(@hash['data']['type']).to eq('merchant')
-      expect(@hash['data']['attributes'].class).to eq(Hash)
-      expect(@hash['data']['attributes'].length).to eq(2)
-      expect(@hash['data']['attributes']['name']).to eq(@merchant_2.name.to_s)
-      expect(@hash['data']['attributes']['id']).to eq(@merchant_2.id)
+      expect(@json['data']['id']).to eq(@merchant_2.id.to_s)
+      expect(@json['data']['type']).to eq('merchant')
+      expect(@json['data']['attributes'].class).to eq(Hash)
+      expect(@json['data']['attributes'].length).to eq(2)
+      expect(@json['data']['attributes']['name']).to eq(@merchant_2.name.to_s)
+      expect(@json['data']['attributes']['id']).to eq(@merchant_2.id)
     end
   end
 end
