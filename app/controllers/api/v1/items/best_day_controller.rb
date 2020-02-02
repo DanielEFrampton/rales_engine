15da819307b10980f882a7edd8391942a910f74f
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-02 15:28:01 -0700

    Refactor best_day controller to use class method instead of instance

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Items::BestDayController < ApplicationController
   def show
-    item = Item.find(params[:id])
-    render json: BestDaySerializer.new(item.best_day).to_hash
+    render json: BestDaySerializer.new(Item.best_day(params[:id])).to_hash
   end
 end
