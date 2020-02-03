85dc89b3e39c398f4791c428ba2bdc8a070cbbc8
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-02 23:53:00 -0700

    Refactor Merchant factory to use wider range of dates

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :merchant do
     name { Faker::Name.name }
-    created_at { Time.zone.parse(Faker::Date.unique.in_date_period.to_s) }
-    updated_at { Time.zone.parse(Faker::Date.unique.in_date_period.to_s) }
+    created_at { Time.zone.parse(Faker::Date.unique.between(from: 10.years.ago, to: 1.year.ago).to_s) }
+    updated_at { Time.zone.parse(Faker::Date.unique.between(from: 10.years.ago, to: 1.year.ago).to_s) }
   end
 end
