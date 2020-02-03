78f0ea68d64f9a2b9831f7534054b265b518d43c
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-03 01:17:07 -0700

    Add route to InvoiceItems::FindController index action

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
       namespace :invoice_items do
         get '/', to: 'invoice_items#index'
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
         get '/:id', to: 'invoice_items#show'
         get '/:id/item', to: 'item#show'
         get '/:id/invoice', to: 'invoice#show'
