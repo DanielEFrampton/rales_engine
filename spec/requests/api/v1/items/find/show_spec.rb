0c80775db9a4ef0d87595ee6361c06384b707bb9
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-02 19:33:13 -0700

    Correct expectations to match spec harness

--- spec/requests/api/v1/items/find/show_spec.rb
+++ spec/requests/api/v1/items/find/show_spec.rb
@@ -122,7 +122,7 @@ RSpec.describe 'As a visitor', type: :request do
 
     describe 'by its description attribute' do
       before(:each) do
-        get "/api/v1/items/find?updated_at=#{@item_3.updated_at}"
+        get "/api/v1/items/find?description=#{@item_3.description}"
         @json = JSON.parse(response.body)
       end
 
@@ -148,7 +148,7 @@ RSpec.describe 'As a visitor', type: :request do
 
     describe 'by its merchant_id attribute' do
       before(:each) do
-        get "/api/v1/items/find?updated_at=#{@item_7.updated_at}"
+        get "/api/v1/items/find?merchant_id=#{@item_7.merchant_id}"
         @json = JSON.parse(response.body)
       end
 
@@ -169,6 +169,33 @@ RSpec.describe 'As a visitor', type: :request do
         expect(attributes['unit_price']).to eq(sprintf('%.2f',
                                                @item_7.unit_price / 100.0))
         expect(attributes['merchant_id']).to eq(@item_7.merchant_id)
+      end
+    end
+
+    describe 'by its unit_price attribute' do
+      before(:each) do
+        formatted_price = sprintf('%.2f', @item_4.unit_price / 100.0)
+        get "/api/v1/items/find?unit_price=#{formatted_price}"
+        @json = JSON.parse(response.body)
+      end
+
+      it 'I get a JSON:API response containing attributes of that item' do
+        expect(@json.class).to eq(Hash)
+        expect(@json.length).to eq(1)
+        expect(@json['data'].length).to eq(3)
+
+        expect(@json['data']['id']).to eq(@item_4.id.to_s)
+        expect(@json['data']['type']).to eq('item')
+
+        attributes = @json['data']['attributes']
+        expect(attributes.length).to eq(5)
+
+        expect(attributes['name']).to eq(@item_4.name)
+        expect(attributes['id']).to eq(@item_4.id)
+        expect(attributes['description']).to eq(@item_4.description)
+        expect(attributes['unit_price']).to eq(sprintf('%.2f',
+                                               @item_4.unit_price / 100.0))
+        expect(attributes['merchant_id']).to eq(@item_4.merchant_id)
       end
     end
   end
