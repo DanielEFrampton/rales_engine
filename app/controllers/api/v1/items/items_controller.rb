6fdf974d067fc236d0904672cb372ee94ebfd71b
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-02 17:53:05 -0700

    Add index action to items-namespaced items controller

--- app/controllers/api/v1/items/items_controller.rb
+++ app/controllers/api/v1/items/items_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Items::ItemsController < ApplicationController
-  def show    
+  def show
     render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def index
+    render json: ItemSerializer.new(Item.all)
   end
 end
