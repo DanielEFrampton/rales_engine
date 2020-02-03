a95a0a8407e14e632f80e1e62dd1653d31e128e6
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-02-02 19:46:35 -0700

    Add route to items find_all endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
         get '/', to: 'items#index'
         get '/most_revenue', to: 'most_revenue#index'
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
         get '/:id', to: 'items#show'
         get '/:id/best_day', to: 'best_day#show'
       end
